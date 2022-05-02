20a26e35105a8325cb4c68f61cc513c8d8decd94
Joel Dixon <joel.dixon@ni.com>
2022-05-02 11:11:19 -0500

    Hello world example plugin building

---
+++ csharp/examples/helloworld/HelloWorldPanelPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using NationalInstruments.InstrumentFramework.Plugins;
+
+namespace NationalInstruments.InstrumentStudio.HelloWorldPlugin
+{
+    internal class HelloWorldPanelPlugin : PanelPlugin
+    {
+        public HelloWorldPanelPlugin(PluginSession pluginSession)
+            : base(pluginSession)
+        {
+        }
+
+        public override FrameworkElement PanelContent => new TextBlock { Text = "Hello InstrumentStudio World" };
+    }
+}
